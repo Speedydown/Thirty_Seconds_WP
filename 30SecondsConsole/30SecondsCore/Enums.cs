674b1adb6b607be7aa0a1d7221e9e5d65efb8500
Teake <Teake@wiezitwaarvandaag.nl>
2016-01-11 15:52:45 +0100

    Eerste stap datastructuur

---
+++ 30SecondsConsole/30SecondsCore/Enums.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30SecondsCore
+{
+    public enum Languages { World, Nederlands, English, Deutch }
+    public enum Categories { Brand, geography, Celebrity, Political, Animals  }
+}
---
+++ 30SecondsConsole/30SecondsCore/Model/Question.cs
@@ -0,0 +1,58 @@
+﻿using _30SecondsCore.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30SecondsCore.Model
+{
+    public class Question //: DataObject
+    {
+        public string Inquiry { get; private set; }
+        public Languages Language { get; private set; }
+        public Categories Category { get; private set; }
+        public DateTime LastEdited { get; private set; }
+        public bool Expired { get; private set; }
+
+        public Question(
+            int ID,
+            string Inquery,
+            int LanguageID,
+            int CategoryID,
+            DateTime LastEdited,
+            bool Expired) :
+        this(
+            ID,
+            Inquery,
+            (Languages)LanguageID,
+            (Categories)CategoryID,
+            LastEdited,
+            Expired)
+        {
+
+        }
+
+        public Question(
+            int ID,
+            string Inquiry,
+            Languages Language,
+            Categories Category,
+            DateTime LastEdited,
+            bool Expired)
+            : base() //TODO : ID
+        {
+            this.Inquiry = Inquiry;
+            this.Language = Language;
+            this.Category = Category;
+            this.LastEdited = LastEdited;
+            this.Expired = Expired;
+
+            //if (this.ID == 0)
+            //{
+            //    LastEdited = TimeConverter.GetDateTime();
+            //    QuestionHandler.instance.AddObject(this);
+            //}
+        }
+    }
+}
